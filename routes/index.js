cb8bdde98ef038b30a2065548a210290e44769fa
Juanzgc <juanzgc@bu.edu>
2018-12-01 13:57:40 -0500

    node modules

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+  Will be used as our controller page
+*/
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
