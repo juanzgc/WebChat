6535a02982979535298199d824824a6408ea80e7
Juanzgc <juanzgc@bu.edu>
2018-12-01 14:31:29 -0500

    Video in the html is up:

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-  Will be used as our controller page
-*/
-
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express'
+  });
 });
 
 module.exports = router;
