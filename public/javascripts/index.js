5123f21c8e1883a9afa08f480ec2fd05e0b5742a
Juanzgc <juanzgc@bu.edu>
2018-12-02 02:18:52 -0500

    Added audo back

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -90,7 +90,7 @@ var localVideo = document.querySelector('#localVideo');
 var remoteVideo = document.querySelector('#remoteVideo');
 
 navigator.mediaDevices.getUserMedia({
-  audio: false,
+  audio: true,
   video: true
 })
 .then(gotStream)
